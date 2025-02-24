7bac6cfe84269a857232029e5cb04206c1ffd295
QamarDev1122 <qamarshehzad773@gmail.com>
2025-02-25 02:15:45 +0500

    fix

--- src/components/EducationSection.js
+++ src/components/EducationSection.js
@@ -1,6 +1,8 @@
 // src/components/EducationSection.js
 import React from 'react';
 import fast from '../assets/images/fastnu.png'
+import PIFD from '../assets/images/pifd.png'
+import LSFD from '../assets/images/lsfd.png'
 
 const EducationSection = () => (
     <section>
@@ -10,22 +12,21 @@ const EducationSection = () => (
             <div class="card-body p-5">
                 <div class="row align-items-center gx-5">
                     <div className="col text-center text-lg-start mb-4 mb-lg-0">
-                        <a href="https://www.nu.edu.pk/" style={{ textDecoration: 'none' }}>
+                        <a href="https://www.eduvision.edu.pk/lahore-school-of-fashion-design-lsfd-lahore-ins-1310990918" style={{ textDecoration: 'none' }}>
                             <div className="bg-light p-4 rounded-4 d-flex align-items-center">
                                 <img 
-                                src={fast} 
+                                src={PIFD} 
                                 alt="FAST-NUCES Logo" 
                                 style={{ width: '70px', height: '70px', objectFit: 'contain', marginRight: '10px', borderRadius: '50px' }} 
                                 />
                                 <div>
                                     <div className="text-primary fw-bolder mb-2">2019 - 2023</div>
                                     <div className="mb-2">
-                                        <div className="small fw-bolder">FAST-NUCES</div>
-                                        <div className="small text-muted">CFD Campus</div>
+                                        <div className="small fw-bolder">Pakistan Institute of Fashion and Design</div>
+                                        <div className="small text-muted">Lahore Campus</div>
                                     </div>
                                     <div className="fst-italic">
-                                        <div className="small text-muted">Undergraduate</div>
-                                        <div className="small text-muted">Computer Science</div>
+                                        <div className="small text-muted">Graphic & Textile Designing</div>
                                     </div>
                                 </div>
                             </div>
@@ -33,7 +34,35 @@ const EducationSection = () => (
                     </div>
                     <div class="col-lg-8">
                         <div>
-                            Acquired a solid foundation in computer science principles and practical experience in software development.
+                            Acquired a solid foundation of Graphics principles and practical experience in fashion designing.
+                        </div>
+                    </div>
+                </div>
+                <div class="row align-items-center gx-5">
+                    <div className="col text-center text-lg-start mb-4 mb-lg-0">
+                        <a href="https://www.eduvision.edu.pk/lahore-school-of-fashion-design-lsfd-lahore-ins-1310990918" style={{ textDecoration: 'none' }}>
+                            <div className="bg-light p-4 rounded-4 d-flex align-items-center">
+                                <img 
+                                src={LSFD} 
+                                alt="FAST-NUCES Logo" 
+                                style={{ width: '70px', height: '70px', objectFit: 'contain', marginRight: '10px', borderRadius: '50px' }} 
+                                />
+                                <div>
+                                    <div className="text-primary fw-bolder mb-2">2019 - 2023</div>
+                                    <div className="mb-2">
+                                        <div className="small fw-bolder">Lahore School Of Fashion Design</div>
+                                        <div className="small text-muted">Lahore Campus</div>
+                                    </div>
+                                    <div className="fst-italic">
+                                        <div className="small text-muted">Fashion and Graphics Designing</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-8">
+                        <div>
+                            Acquired a solid foundation of Graphics principles and practical experience in fashion designing.
                         </div>
                     </div>
                 </div>
